a775da5351e9205ccd0aab335ca7e23ce23ba95a
Jessel Sookha <jsookha@gmail.com>
2024-03-07 12:45:07 +0200

    first commit

---
+++ src/coniprgarrays/ConIPRGArrays.java
@@ -0,0 +1,28 @@
+/*
+Program: 1D Arrays 
+Date: 
+Programmer: J. Sookha 
+Description: 
+
+*/
+package coniprgarrays;
+
+public class ConIPRGArrays {
+    static int intCount; 
+    static int[] intNumArr = new int[10];
+    
+    public static void main(String[] args) {
+        
+        for (intCount = 0; intCount <= 9; intCount++)
+        {
+            intNumArr[intCount] = intCount;  
+        }
+        
+        int intSum; 
+        float fltAvg; 
+        
+        intSum = 0; 
+        
+    }
+    
+}
